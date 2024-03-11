af59b4c1f7e1e3ef055c07f79fae80f74296ffae
Farid Dibirov <farid962@gmail.com>
2024-03-11 16:18:02 +0400

    modelstate validation, httpdelete

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -37,11 +37,23 @@ public class VillaAPIController : ControllerBase
 	}
 
 	[HttpPost]
-	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status201Created)]
 	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
 	public ActionResult<VillaDTO>  CreateVilla([FromBody]VillaDTO villaDTO)
 	{
+		//if(!ModelState.IsValid)
+		//{
+		//	return BadRequest(ModelState);
+		//}
+
+		if (VillaStore.VillaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null) 
+		{
+			ModelState.AddModelError("CustomError", "Villa already exist!");
+
+			return BadRequest(ModelState);
+		}
+
 		if(villaDTO==null)
 		{
 			return BadRequest(villaDTO);
@@ -59,4 +71,27 @@ public class VillaAPIController : ControllerBase
 		return CreatedAtRoute("GetVilla", new { id =villaDTO.Id}, villaDTO );
 
 	}
+
+	[HttpDelete("{id:int}", Name = "DeleteVilla")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	public IActionResult DeleteVilla(int id)
+	{
+		if (id==0)
+		{
+			return BadRequest();
+		}
+
+		var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
+
+		if(villa==null)
+		{
+			return NotFound();
+		}
+		VillaStore.VillaList.Remove(villa);
+
+		return NoContent();
+	}
+
 }
