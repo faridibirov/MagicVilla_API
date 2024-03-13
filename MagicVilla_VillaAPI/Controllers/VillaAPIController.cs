09c9dbaf05af464a04a74acb641f8cc067651caa
Farid Dibirov <farid962@gmail.com>
2024-03-13 15:00:28 +0400

    logger, serilog, ilogging

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Logging;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
@@ -10,10 +11,18 @@ namespace MagicVilla_VillaAPI.Controllers;
 [ApiController]
 public class VillaAPIController : ControllerBase
 {
+	private readonly ILogging _logger;
+
+	public VillaAPIController(ILogging logger)
+	{
+		_logger = logger;
+	}
+
 	[HttpGet]
 	[ProducesResponseType(StatusCodes.Status200OK)]
 	public ActionResult <IEnumerable<VillaDTO>> GetVillas()
 	{
+		_logger.Log("Getting all villas", "");
 		return Ok(VillaStore.VillaList);
 	}
 
@@ -25,6 +34,7 @@ public class VillaAPIController : ControllerBase
 	{
 		if(id==0)
 		{
+			_logger.Log("Get Villa Error with Id " + id, "error");
 			return BadRequest();
 ;		}
 		var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
